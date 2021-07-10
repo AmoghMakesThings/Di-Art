ef7301d21caf75aafaf225e91a111459821a282c
AmoghMakesThings <85697969+AmoghMakesThings@users.noreply.github.com>
2021-07-10 15:19:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+function test() {
+    document.getElementById("test").src="elephant.jpg"
+}
+function test2(){
+    document.getElementById("test").src="LIGHT.jpg"
+}
+function test3() {
+    document.getElementById("img2").src="buddha.jpg"
+}
+function test4() {
+    document.getElementById("img2").src="man.jpg"
+}
+function test5() {
+    document.getElementById("img3").src="woman.jpg"
+}
+function test6() {
+    document.getElementById("img3").src="SUNSET.jpg"
+}
+
